83871a70c62aef4da0e71e03899435f2e04020aa
juanpchica <juanpchica@hotmail.com>
2021-01-14 14:34:30 -0500

    Some styles added

--- src/bookList.js
+++ src/bookList.js
@@ -2,16 +2,30 @@ export const bookList = [
   {
     id: 1,
     img: "https://m.media-amazon.com/images/I/51tP+MNLRQL.jpg",
-    title:
-      "Unlimited PowerUnlimited PowerUnlimited Power Anthony Robbins, Simon & Schuster AudioAnthony Robbins, Simon & Schuster Audio",
+    title: "Unlimited PowerUnlimited PowerUnlimited",
     author: "Audible Audiolibro",
   },
   {
     id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/41pIJKMo8RL._AC_SX184_.jpg",
-    title:
-      "Where to Begin: A Small Book About Your Power to Create Big ChangeWhere to Begin: A Small Book About Your Power to Create Big ChangeWhere to Begin: A Small Book About Your Power",
+    title: "Where to Begin: A Small Book About Your Power to Create",
     author: "Cleo Wade",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51ydM3eSuZL._AC_SX184_.jpg",
+    title:
+      "The Astrotwins' 2021 Horoscope: The Complete Yearly Astrology Guide for Every Zodiac The Astrotwins' 2021",
+    author: "Tapa blanda",
+  },
+  {
+    id: 4,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51KkyqK7avL._AC_SX184_.jpg",
+    title:
+      "After the Rain: Gentle Reminders for Healing, Courage, and Self-LoveAfter the Rain:",
+    author: "Katherine May",
   },
 ];
